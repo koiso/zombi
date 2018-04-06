6af6f427385a287e71f6e8f231c03733a4f90b86
koiso <heppu.seuranen@gmail.com>
2018-04-06 15:15:43 +0300

    First commit, working BLEscan with coordinates from phone.
    TODO: add coordinates + MAC address of the BLE node from scan to SQLite database.
    
    Get RSSI values from nodes and upgrade databse if smaller than earlier (accuracy is increasing if you get closer to node)

---
+++ app/src/main/java/koti/blescanapi21/LocationFetch.java
@@ -0,0 +1,170 @@
+package koti.blescanapi21;
+
+import android.app.Activity;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+import android.widget.Toast;
+
+/**
+ * Created by Proot on 5.4.2018.
+ */
+
+public class LocationFetch extends Service implements LocationListener {
+
+    private Context context;
+    private Activity activity;
+    protected LocationManager locationManager;
+
+    Location location;
+    double latitude;
+    double longitude;
+
+    private String NLOC;
+    private String ELOC;
+
+    private boolean gps_on = false;
+    private boolean net_on = false;
+
+/*
+    public LocationFetch(){
+
+    }
+
+    public LocationFetch(Context context, Activity activity){
+        this.context = context;
+        this.activity = activity;
+        //locationManager = (LocationManager) this.context.getSystemService(context.LOCATION_SERVICE);
+    }
+    */
+
+    public void onCreate() {
+        Log.d("JALAJALA", "LOCATIONFETCH_CREATE");
+        locationManager = (LocationManager) this.getSystemService(LOCATION_SERVICE);
+        //LocalBroadcastManager broadcaster = LocalBroadcastManager.getInstance(this);
+        try {
+            Log.d("JALAJALA", "OIKEUDET_ON");
+            //locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 2000, 10, this);
+
+            gps_on = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+            net_on = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+            Log.d("JALAJALA GPS_ON: ", String.valueOf(gps_on));
+            Log.d("JALAJALA NET_ON: ", String.valueOf(net_on));
+
+            if (location == null) {
+                Log.d("JALAJALA: ", "Location NULL");
+
+                if (gps_on) {
+                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 2000, 10, this);
+                    //location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                } else {
+                    Log.d("JALAJALA: ", "NETWORK PROVIDER REQUEST");
+                    locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 2000, 10, this);
+                }
+                if (locationManager != null) {
+                    Log.d("JALAJALA: ", "LocationManager NOT NULL");
+
+                    if (gps_on) {
+                        location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                    } else {
+                        location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                    }
+                    //NLOC = String.valueOf(location.getLatitude());
+                    //ELOC = String.valueOf(location.getLongitude());
+                    //sendResults();
+
+                    if (location != null) {
+                        Log.d("JALAJALA: ", "Location NOT NULL");
+                        //latitude = location.getLatitude();
+                        //longitude = location.getLongitude();
+                        NLOC = String.valueOf(location.getLatitude());
+                        ELOC = String.valueOf(location.getLongitude());
+                        sendResults();
+                    }
+                }
+            }
+
+        } catch (SecurityException e) {
+            Log.d("JALAJALA", e.toString());
+        }
+    }
+
+    public void onStart(Intent intent, int startID) {
+        super.onStart(intent, startID);
+    }
+
+    public int onStartCommand(Intent intent, int flags, int startID) {
+        Log.d("JALAJALA", "LOCATIONFETCH_ONSTARTCOMMAND");
+        super.onStartCommand(intent, flags, startID);
+        return START_NOT_STICKY;
+    }
+
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+
+    public double getLocN() {
+        if (location != null) {
+            latitude = location.getLatitude();
+        }
+        return latitude;
+    }
+
+    public double getLocE() {
+        if (location != null) {
+            longitude = location.getLongitude();
+        }
+        return longitude;
+    }
+
+    public void sendResults() {
+        Intent intent = new Intent("JALAEVENTNAME");
+        //intent.putExtra("NLOC", String.valueOf(location.getLatitude()));
+        //intent.putExtra("ELOC", String.valueOf(location.getLongitude()));
+        intent.putExtra("NLOC", NLOC);
+        intent.putExtra("ELOC", ELOC);
+
+        LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
+    }
+
+    public void onLocationChanged(Location location) {
+        String message = "N: " + location.getLatitude() + "\nE: " + location.getLongitude();
+        //Toast.makeText(this.context, message, Toast.LENGTH_LONG).show();
+        Log.d("JALA", message);
+        NLOC = String.valueOf(location.getLatitude());
+        ELOC = String.valueOf(location.getLongitude());
+        sendResults();
+    }
+
+    public void onProviderDisabled(String provider) {
+        //Intent intentti = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+        //startActivity(intentti);
+        Log.d("JALAJALA", "GPS IS TURNED OFF");
+        Toast.makeText(this, "GPS IS TURNED OFF, PLEASE TURN IT ON TO USE THIS APP", Toast.LENGTH_SHORT).show();
+
+    }
+
+    public void onProviderEnabled(String provider) {
+        Log.d("JALAJALA", "GPS IS ON");
+        Toast.makeText(this, "GPS IS ON", Toast.LENGTH_SHORT).show();
+    }
+
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        //Something something
+    }
+
+    @Override
+    public IBinder onBind(Intent arg0) {
+        return null;
+    }
+
+}
---
+++ app/src/main/java/koti/blescanapi21/MainActivity.java
@@ -0,0 +1,189 @@
+package koti.blescanapi21;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.le.BluetoothLeScanner;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanResult;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
+import android.os.Handler;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.LocalBroadcastManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.util.Log;
+import android.Manifest;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BluetoothAdapter mBluetoothAdapter;
+    private LocationManager locationManager;
+
+    private Handler mHandler = new Handler();
+    private boolean mScanning = true;
+
+    private final static long SCAN_PERIOD = 10000;
+    private final static int REQUEST_ENABLE_BT = 1;
+
+    String deviceAddress;
+    TextView teksti;
+
+    LocationFetch locationFetch;
+    private String locN;
+    private String locE;
+
+    final boolean enable = true;
+
+    //receiver for LocationFetch location broadcasts
+    private BroadcastReceiver mMessageReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.d("JALAJALA: ", "RECEIVER CHECK");
+            locN = intent.getStringExtra("NLOC");
+            locE = intent.getStringExtra("ELOC");
+        }
+    };
+
+    //replies from scan
+    private ScanCallback mLeScanCallback = new ScanCallback() {
+        @Override
+        public void onScanResult(int callbackType, ScanResult result) {
+            super.onScanResult(callbackType, result);
+            //Log.d("JALAJALA: ", result.toString());
+            Log.d("JALAJALA: ", "ONSCANRESULT");
+            BluetoothDevice bluetoothDevice = result.getDevice();
+
+            deviceAddress = bluetoothDevice.getAddress();
+            //String deviceName = bluetoothDevice.getName();
+
+            //Log.d("JALAJALA", deviceName);
+            if (deviceAddress != null) {
+                asetaTeksti(deviceAddress);
+            }
+            //Log.d("JALAJALA", deviceName);
+        }
+
+        @Override
+        public void onBatchScanResults(List<ScanResult> results) {
+            super.onBatchScanResults(results);
+            Log.d("JALAJALA: ", "ONBATCHRESULTS");
+        }
+
+        @Override
+        public void onScanFailed(int errorCode) {
+            super.onScanFailed(errorCode);
+            Log.d("JALAJALA: ", "ONSSCANFAILED");
+        }
+    };
+
+    //method to set MAC and location to textview
+    public void asetaTeksti(String deviceAddress) {
+        Log.d("JALAJALA ", deviceAddress);
+        teksti = (TextView)findViewById(R.id.textView);
+
+        if (teksti == null) {
+            teksti.setText("MAC: " + deviceAddress + "\nN: " + locN + "\nE: " + locE + "\n\n");
+        }
+        else {
+            teksti.setText(teksti.getText(), TextView.BufferType.EDITABLE);
+            ((Editable) teksti.getText()).insert(0, "MAC: " + deviceAddress + "\nN: " + locN + "\nE: " + locE + "\n\n");
+        }
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        //basic
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Broadcastlisterner
+        LocalBroadcastManager.getInstance(this).registerReceiver(mMessageReceiver, new IntentFilter("JALAEVENTNAME"));
+
+        BluetoothManager bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        mBluetoothAdapter = bluetoothManager.getAdapter();
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        //if bt not enabled, ask to enable it
+        if (mBluetoothAdapter == null || !mBluetoothAdapter.isEnabled()) {
+            Intent enableBtintent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtintent, REQUEST_ENABLE_BT);
+        }
+
+        //If there is no rights for location:
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding-
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.'
+
+            Log.d("JALAJALA: ","LOC OIKEUS_CHECK");
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+
+        }
+        else {
+            Log.d("JALAJALA: ", "LOC OIKEUDET_ON");
+            startService(new Intent(this, LocationFetch.class));
+
+
+            scanLeDevice(enable);
+        }
+
+        Button cameraButton = (Button) findViewById(R.id.button);
+        cameraButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                teksti.setText(" ");
+                scanLeDevice(enable);
+
+            }
+        });
+    }
+
+    //scanner for BLE nodes, takes boolen true to start scanning. SCAN_PERIOD is cycle for one scan
+    private void scanLeDevice(final boolean enable) {
+
+        final BluetoothLeScanner bluetoothLeScanner = mBluetoothAdapter.getBluetoothLeScanner();
+
+        if (enable) {
+            // Stops scanning after a pre-defined scan period.
+            mHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    mScanning = false;
+
+                    bluetoothLeScanner.stopScan(mLeScanCallback);
+                }
+            }, SCAN_PERIOD);
+
+            mScanning = true;
+            bluetoothLeScanner.startScan(mLeScanCallback);
+        } else {
+            mScanning = false;
+            bluetoothLeScanner.stopScan(mLeScanCallback);
+        }
+    }
+
+
+
+
+}
